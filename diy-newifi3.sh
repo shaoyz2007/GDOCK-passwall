30e66c21a9d352f40e4596ff405494ddb7e5990c
shaoyz2007 <61934974+shaoyz2007@users.noreply.github.com>
2020-03-13 18:32:13 +0800

    delete i'dont need

--- diy-newifi3.sh
+++
@@ -1,8 +0,0 @@
-echo '修改时区'
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-
-echo '修改banner'
-rm -rf package/base-files/files/etc/banner
-cp -f ../banner-newifi3 package/base-files/files/etc/banner
-
-
